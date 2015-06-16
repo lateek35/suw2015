77db319aed3cf492a26a59426e8220f46fccf04a
Allan RAQUIN <lateek35@gmail.com>
2015-06-16 18:24:12 +0200

    change project's base

--- www/js/app.js
+++ www/js/app.js
@@ -3,92 +3,81 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var facebookApp = angular.module("starter", ["ionic", "ngStorage", "ngCordova"])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives', 'firebase','ngResource'])
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
     }
   });
 })
 
-facebookApp.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('login', {
-            url: '/login',
-            templateUrl: 'templates/login.html',
-            controller: 'LoginController'
-        })
-        .state('profile', {
-            url: '/profile',
-            templateUrl: 'templates/profile.html',
-            controller: 'ProfileController'
-        })
-        .state('friends', {
-            url: '/friends',
-            templateUrl: 'templates/friends.html',
-            controller: 'FriendsController'
-        });
-    $urlRouterProvider.otherwise('/login');
-});
+.config(function($stateProvider, $urlRouterProvider) {
 
-facebookApp.controller("LoginController", function($scope, $cordovaOauth, $localStorage, $location) {
- 
-    $scope.login = function() {
-      // if(!$localStorage.hasOwnProperty("accessToken") === true && $localStorage.accessToken==="") {
-        $cordovaOauth.facebook("763024983807122", ["email", "read_stream", "user_website", "user_location", "user_relationships", "user_friends"]).then(function(result) {
-            $localStorage.accessToken = result.access_token;
-            $location.path("/profile");
-        }, function(error) {
-            alert("There was a problem signing in!  See the console for logs");
-            console.log(error);
-        });
-      // }else{
-      //   $location.path("/profile");
-      // }
-    };
- 
-});
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-facebookApp.controller("ProfileController", function($scope, $http, $localStorage, $location) {
- 
-    $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true && $localStorage.accessToken!=="") {
-            $http.get("https://graph.facebook.com/v2.3/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status,email", format: "json" }}).then(function(result) {
-                $scope.profileData = result.data;
-            }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
-                console.log(error);
-            });
-        } else {
-            $location.path("/login");
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
         }
-    };
- 
-});
-
-facebookApp.controller("FriendsController", function($scope, $http, $localStorage, $location) {
- 
-    $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true && $localStorage.accessToken!=="") {
-            $http.get("https://graph.facebook.com/v2.3/me/taggable_friends?limit=1000", { params: { access_token: $localStorage.accessToken, format: "json" }}).then(function(result) {
-                $scope.friendsData = result.data.data;
-                // $http.get("https://graph.facebook.com/v2.3/me/taggable_friends?limit=1000", { params: { access_token: $localStorage.accessToken, fields: "picture", format: "json" }}).then(function(result) {
-                //     $scope.friendsData.picture = result.data.picture.data.url;
-                // });
-            }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
-                console.log(error);
-                $scope.friendsData = error;
-            });
-        } else {
-            $location.path("/login");
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'chatController'
         }
-    };
- 
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
 });
---
+++ www/js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $resource, Bar, Camera) {
+  // $scope.getPhoto = function() {
+  //   Camera.getPicture().then(function(imageURI) {
+  //     console.log(imageURI);
+  //   }, function(err) {
+  //     console.err(err);
+  //   });
+  // };
+
+    $scope.bars = Bar.query();
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('chatController', ["$scope", "chatMessages","$ionicScrollDelegate", function($scope, chatMessages, $ionicScrollDelegate ) {
+    //Set messages to chatMessages factory which returns the firebase data
+    $scope.messages = chatMessages;
+    
+    //Initialize message object
+    $scope.message = {};
+ 
+    //Add message to the firebase data
+    $scope.addMessage = function(message) {
+      $scope.messages.$add({content: message, name:"aan"});
+      //we reset the text input field to an empty string
+      $scope.message.theMessage = "";
+      $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom(true);
+    };
+
+
+}])
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+
